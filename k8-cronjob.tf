49e4d6b810e126c21d662ae3f8b9d60d3874b9d5
Shaung <shaungc@umich.edu>
2019-08-24 23:32:25 -0700

    finish added k8 cron job for database backup, run daily, closes issue rivernews/iriversland2-api#14

--- k8-cronjob.tf
+++ k8-cronjob.tf
@@ -1,26 +1,41 @@
+# tf spec: https://www.terraform.io/docs/providers/kubernetes/r/cron_job.html
+# k8 spec: https://kubernetes.io/docs/tasks/job/automated-tasks-with-cron-jobs/
 resource "kubernetes_cron_job" "db_backup_cron" {
   metadata {
     name = "db-backup-cronjob"
     namespace = "${var.cicd_namespace}"
   }
   spec {
-    concurrency_policy            = "Forbid"
+    concurrency_policy            = "Replace"
     failed_jobs_history_limit     = 10
-    schedule                      = "1 * * * *"
-    starting_deadline_seconds     = 10
-    successful_jobs_history_limit = 10
-    suspend                       = true
+    successful_jobs_history_limit = 5
+    schedule                      = "0 0 * * *" # every day 00:00
+    starting_deadline_seconds     = 5
+
     job_template {
-      metadata {}
       spec {
-        backoff_limit = 1
+
+        # https://kubernetes.io/docs/concepts/workloads/controllers/jobs-run-to-completion/#job-termination-and-cleanup
+        active_deadline_seconds = 60 # max job alive time
+        
+        # https://kubernetes.io/docs/concepts/workloads/controllers/jobs-run-to-completion/#pod-backoff-failure-policy
+        backoff_limit = 1 # no retry
+
         template {
           metadata {}
           spec {
             container {
               name    = "db-backup-container"
               image   = "${var.app_container_image}:${var.app_container_image_tag}"
-              command = ["/bin/sh", "-c", "cd /usr/src/backend && python manage.py backup_db"]
+              command = ["/bin/sh", "-c", "echo Starting cron job... && sleep 5 && cd /usr/src/backend && echo Finish CD && python manage.py backup_db && echo Finish dj command"]
+
+              dynamic "env" {
+                for_each = local.app_secret_key_value_pairs
+                content {
+                  name  = env.key
+                  value = env.value
+                }
+              }
             }
 
             # tf doc: https://www.terraform.io/docs/providers/kubernetes/r/deployment.html#template-spec
