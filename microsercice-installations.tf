c3e3a9d6c00912191b5cee4a1ae5babf38de5084
Shaung <shaungc@umich.edu>
2019-09-07 01:09:18 -0700

    let circleci build success by feeding default app build tags

--- microsercice-installations.tf
+++ microsercice-installations.tf
@@ -1,43 +1,3 @@
-module "appl_tracky_api" {
-  source = "./microservice-installation-module"
-  
-  # cluster-wise config (shared resources across different microservices)
-  dockerhub_kubernetes_secret_name = "${kubernetes_secret.dockerhub_secret.metadata.0.name}"
-  cert_cluster_issuer_name = "${local.cert_cluster_issuer_name}"
-  tls_cert_covered_domain_list = local.tls_cert_covered_domain_list
-  cert_cluster_issuer_k8_secret_name = "${local.cert_cluster_issuer_k8_secret_name}"
-  
-  # app-specific config (microservice)
-  app_label               = "appl-tracky-api"
-  app_exposed_port        = 8001
-  app_deployed_domain     = "appl-tracky.api.shaungc.com"
-  app_container_image     = "shaungc/appl-tracky-api"
-  app_container_image_tag = "${var.appl_tracky_api_image_tag}"
-  app_secret_name_list = [
-    "/app/appl-tracky/DJANGO_SECRET_KEY",
-    "/app/appl-tracky/ADMINS",
-
-    "/database/heroku_appl-tracky/SQL_ENGINE",
-    "/database/heroku_appl-tracky/SQL_DATABASE",
-    "/database/heroku_appl-tracky/SQL_USER",
-    "/database/heroku_appl-tracky/SQL_PASSWORD",
-    "/database/heroku_appl-tracky/SQL_HOST",
-    "/database/heroku_appl-tracky/SQL_PORT",
-
-    "/service/gmail/EMAIL_HOST",
-    "/service/gmail/EMAIL_HOST_USER",
-    "/service/gmail/EMAIL_HOST_PASSWORD",
-    "/service/gmail/EMAIL_PORT",
-
-    "/service/google-social-auth/SOCIAL_AUTH_GOOGLE_OAUTH2_KEY",
-    "/service/google-social-auth/SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET",
-  ]
-}
-
-
-
-
-
 module "iriversland2_api" {
   source = "./microservice-installation-module"
   
@@ -81,5 +41,42 @@ module "iriversland2_api" {
           cron_schedule = "* * * * *", # every day 00:00 PST
           command = ["/bin/sh", "-c", "echo Starting cron job... && sleep 5 && cd /usr/src/backend && echo Finish CD && python manage.py backup_db && echo Finish dj command"]
       },
+  ]
+}
+
+
+module "appl_tracky_api" {
+  source = "./microservice-installation-module"
+  
+  # cluster-wise config (shared resources across different microservices)
+  dockerhub_kubernetes_secret_name = "${kubernetes_secret.dockerhub_secret.metadata.0.name}"
+  cert_cluster_issuer_name = "${local.cert_cluster_issuer_name}"
+  tls_cert_covered_domain_list = local.tls_cert_covered_domain_list
+  cert_cluster_issuer_k8_secret_name = "${local.cert_cluster_issuer_k8_secret_name}"
+  
+  # app-specific config (microservice)
+  app_label               = "appl-tracky-api"
+  app_exposed_port        = 8001
+  app_deployed_domain     = "appl-tracky.api.shaungc.com"
+  app_container_image     = "shaungc/appl-tracky-api"
+  app_container_image_tag = "${var.appl_tracky_api_image_tag}"
+  app_secret_name_list = [
+    "/app/appl-tracky/DJANGO_SECRET_KEY",
+    "/app/appl-tracky/ADMINS",
+
+    "/database/heroku_appl-tracky/SQL_ENGINE",
+    "/database/heroku_appl-tracky/SQL_DATABASE",
+    "/database/heroku_appl-tracky/SQL_USER",
+    "/database/heroku_appl-tracky/SQL_PASSWORD",
+    "/database/heroku_appl-tracky/SQL_HOST",
+    "/database/heroku_appl-tracky/SQL_PORT",
+
+    "/service/gmail/EMAIL_HOST",
+    "/service/gmail/EMAIL_HOST_USER",
+    "/service/gmail/EMAIL_HOST_PASSWORD",
+    "/service/gmail/EMAIL_PORT",
+
+    "/service/google-social-auth/SOCIAL_AUTH_GOOGLE_OAUTH2_KEY",
+    "/service/google-social-auth/SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET",
   ]
 }
