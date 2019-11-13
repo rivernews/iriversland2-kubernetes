6f7d70460579788e64595ec6558655c105afcc40
Shaung Cheng <shaungc@umich.edu>
2019-11-12 17:16:31 -0800

    Revision of helm installation for es

--- elastic-stack.tf
+++ elastic-stack.tf
@@ -1,17 +1,22 @@
 # based on tutorial: https://logz.io/blog/deploying-the-elk-stack-on-kubernetes-with-helm/
+# another established tutorial by linode: https://www.linode.com/docs/applications/containers/how-to-deploy-the-elastic-stack-on-kubernetes/
 
 data "helm_repository" "elasticsearch" {
-  name = "elasticsearch"
+  name = "elastic"
   url  = "https://Helm.elastic.co"
+}
+
+data "http" "elasticsearch_helm_chart_values" {
+  url = "put the file url here"
 }
 
 resource "helm_release" "elasticsearch" {
   name       = "elasticsearch-release"
   repository = data.helm_repository.stable.metadata[0].name
-  chart      = "elastic/elasticsearch"
+  chart      = "elasticsearch"
   # version    = "6.0.1" # TODO: lock down version after this release works
 
   values = [
-    "${file("https://raw.githubusercontent.com/elastic/Helm-charts/master/elasticsearch/examples/minikube/values.yaml")}"
+    "${data.http.elasticsearch_helm_chart_values.body}"
   ]
 }
