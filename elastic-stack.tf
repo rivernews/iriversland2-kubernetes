c38b28e64de4a179e44018982c2b6e5737829587
Shaung Cheng <shaungc@umich.edu>
2019-11-12 17:45:57 -0800

    Fix to adapt to digitalocean for es helm chart
    and our use case for es helm

--- elastic-stack.tf
+++ elastic-stack.tf
@@ -1,13 +1,15 @@
 # based on tutorial: https://logz.io/blog/deploying-the-elk-stack-on-kubernetes-with-helm/
 # another established tutorial by linode: https://www.linode.com/docs/applications/containers/how-to-deploy-the-elastic-stack-on-kubernetes/
 
+# helm release terraform doc: https://www.terraform.io/docs/providers/helm/release.html
 data "helm_repository" "elasticsearch" {
   name = "elastic"
   url  = "https://Helm.elastic.co"
 }
 
 data "http" "elasticsearch_helm_chart_values" {
-  url = "put the file url here"
+  # other choices of values.yaml available at: https://github.com/elastic/helm-charts/tree/master/elasticsearch
+  url = "https://raw.githubusercontent.com/elastic/helm-charts/master/elasticsearch/examples/kubernetes-kind/values.yaml"
 }
 
 resource "helm_release" "elasticsearch" {
@@ -19,4 +21,15 @@ resource "helm_release" "elasticsearch" {
   values = [
     "${data.http.elasticsearch_helm_chart_values.body}"
   ]
+  
+  # all available configurations: https://github.com/elastic/helm-charts/tree/master/elasticsearch#configuration
+  set_string {
+    name  = "imageTag"
+    value = "7.4.1" # lock down to version 7.4.1 of Elasticsearch
+  }
+  
+  set_string {
+    name  = "volumeClaimTemplate.resources.requests.storage"
+    value = "1Gi" # DigitalOcean block storage requires at least 1G claim
+  }
 }
