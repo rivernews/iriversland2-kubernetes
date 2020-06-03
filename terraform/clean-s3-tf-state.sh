422ebb6666e2689114f4661a1357cf728e5fdead
Shaung Cheng <shaungc@umich.edu>
2020-06-02 18:58:47 -0700

    real apply

--- terraform/clean-s3-tf-state.sh
+++ terraform/clean-s3-tf-state.sh
@@ -7,7 +7,7 @@ if [[ -z "$TF_VAR_aws_access_key" || -z "$TF_VAR_aws_secret_key" || -z "$TF_VAR_
     echo "One or more aws credentials are not provided, try to use local config"
     docker run --rm -ti -v ~/.aws:/root/.aws amazon/aws-cli --profile local \
         s3 rm s3://iriversland-cloud/terraform/kubernetes/ \
-        --exclude '*' --include '*.tfstate' --recursive --dryrun
+        --exclude '*' --include '*.tfstate' --recursive
 else
     docker run --rm -ti \
         --env AWS_ACCESS_KEY_ID=$TF_VAR_aws_access_key \
@@ -15,5 +15,5 @@ else
         --env AWS_DEFAULT_REGION=$TF_VAR_aws_region \
         amazon/aws-cli \
         s3 rm s3://iriversland-cloud/terraform/kubernetes/ \
-        --exclude '*' --include '*.tfstate' --recursive --dryrun
+        --exclude '*' --include '*.tfstate' --recursive
 fi
