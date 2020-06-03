08dc7620f1e18a697ffc448ad3c8040c3de9694d
Shaung Cheng <shaungc@umich.edu>
2020-06-02 18:53:54 -0700

    send env var into awscli container

--- terraform/clean-s3-tf-state.sh
+++ terraform/clean-s3-tf-state.sh
@@ -9,10 +9,11 @@ if [[ -z "$TF_VAR_aws_access_key" || -z "$TF_VAR_aws_secret_key" || -z "$TF_VAR_
         s3 rm s3://iriversland-cloud/terraform/kubernetes/ \
         --exclude '*' --include '*.tfstate' --recursive --dryrun
 else
-    AWS_ACCESS_KEY_ID=$TF_VAR_aws_access_key \
-    AWS_SECRET_ACCESS_KEY=$TF_VAR_aws_secret_key \
-    AWS_DEFAULT_REGION=$TF_VAR_aws_region \
-    docker run --rm -ti amazon/aws-cli \
+    docker run --rm -ti \
+        --env AWS_ACCESS_KEY_ID=$TF_VAR_aws_access_key \
+        --env AWS_SECRET_ACCESS_KEY=$TF_VAR_aws_secret_key \
+        --env AWS_DEFAULT_REGION=$TF_VAR_aws_region \
+        amazon/aws-cli \
         s3 rm s3://iriversland-cloud/terraform/kubernetes/ \
         --exclude '*' --include '*.tfstate' --recursive --dryrun
 fi
