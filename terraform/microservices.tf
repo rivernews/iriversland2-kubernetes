aedacb43c2c1afc20bf387e1724cc5f59f3d835f
Shaung Cheng <shaungc@umich.edu>
2020-09-26 22:52:14 -0700

    enable postgres in base k8s cluster

--- terraform/microservices.tf
+++ terraform/microservices.tf
@@ -1,28 +1,28 @@
-# module "postgres_cluster" {
-#   source  = "rivernews/kubernetes-microservice/digitalocean"
-#   version = ">= v0.1.13"
+module "postgres_cluster" {
+  source  = "rivernews/kubernetes-microservice/digitalocean"
+  version = ">= v0.1.13"
 
-#   aws_region     = var.aws_region
-#   aws_access_key = var.aws_access_key
-#   aws_secret_key = var.aws_secret_key
-#   cluster_name   = digitalocean_kubernetes_cluster.project_digitalocean_cluster.name
+  aws_region     = var.aws_region
+  aws_access_key = var.aws_access_key
+  aws_secret_key = var.aws_secret_key
+  cluster_name   = digitalocean_kubernetes_cluster.project_digitalocean_cluster.name
 
-#   app_label           = "postgres-cluster"
-#   app_exposed_port    = 5432
-#   app_deployed_domain = ""
+  app_label           = "postgres-cluster"
+  app_exposed_port    = 5432
+  app_deployed_domain = ""
 
-#   app_container_image     = "shaungc/postgres-cdc"
-#   app_container_image_tag = var.postgres_cluster_image_tag
+  app_container_image     = "shaungc/postgres-cdc"
+  app_container_image_tag = var.postgres_cluster_image_tag
 
-#   app_secret_name_list = [
-#     "/database/postgres_cluster_kubernetes/SQL_DATABASE",
-#     "/database/postgres_cluster_kubernetes/SQL_USER",
-#     "/database/postgres_cluster_kubernetes/SQL_PASSWORD",
-#     "/database/postgres_cluster_kubernetes/SQL_DATA_VOLUME_MOUNT",
-#   ]
+  app_secret_name_list = [
+    "/database/postgres_cluster_kubernetes/SQL_DATABASE",
+    "/database/postgres_cluster_kubernetes/SQL_USER",
+    "/database/postgres_cluster_kubernetes/SQL_PASSWORD",
+    "/database/postgres_cluster_kubernetes/SQL_DATA_VOLUME_MOUNT",
+  ]
 
-#   persistent_volume_mount_path_secret_name_list = ["/database/postgres_cluster_kubernetes/SQL_DATA_VOLUME_MOUNT"]
-# }
+  persistent_volume_mount_path_secret_name_list = ["/database/postgres_cluster_kubernetes/SQL_DATA_VOLUME_MOUNT"]
+}
 
 
 module "redis_cluster" {
