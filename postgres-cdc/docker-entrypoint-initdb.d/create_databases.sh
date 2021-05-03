3b6daafd274ee9abae2bdba6e4d1756b9c75f88d
Shaung Cheng <shaungc@umich.edu>
2021-05-03 20:07:20 +0000

    test  commit

--- postgres-cdc/docker-entrypoint-initdb.d/create_databases.sh
+++ postgres-cdc/docker-entrypoint-initdb.d/create_databases.sh
@@ -1,7 +1,7 @@
 psql -U $POSTGRES_USER $POSTGRES_DB -tc "SELECT 1 FROM pg_database WHERE datname = 'appl_tracky_database'" | grep -q 1 || psql -U $POSTGRES_USER $POSTGRES_DB -c "CREATE DATABASE appl_tracky_database"
 
 psql -U $POSTGRES_USER $POSTGRES_DB -tc "SELECT 1 FROM pg_database WHERE datname = 'iriversland2_database'" | grep -q 1 || psql -U $POSTGRES_USER $POSTGRES_DB -c "CREATE DATABASE iriversland2_database"
-
+...
 # testing in psql
 # \list --> list databases
 # \c database_name --> choose active database
