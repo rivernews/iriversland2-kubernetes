5495fb777da9341cb2374ba240e9ba99ea6e8577
Shaung Cheng <shaungc@umich.edu>
2021-06-30 14:33:17 -0700

    bump micro svc module version

--- terraform/code-server.tf
+++ terraform/code-server.tf
@@ -1,6 +1,6 @@
 module "code_server" {
   source  = "rivernews/kubernetes-microservice/digitalocean"
-  version = ">= v0.1.21"
+  version = ">= v0.1.22"
 
   aws_region     = var.aws_region
   aws_access_key = var.aws_access_key
