7f1b5f4ab3aedf9339f90d7bc26b1901d6b4c880
Shaung Cheng <shaungc@umich.edu>
2021-07-01 15:24:49 -0700

    Annotation `certmanager.k8s.io/disable-validation` no longer needed since v0.10, and also webhook seems recommended to be enable (also by default); see issue https://github.com/jetstack/cert-manager/issues/2084 and v0.10 release note https://cert-manager.io/docs/release-notes/release-notes-0.10/

--- terraform/cert-manager.tf
+++ terraform/cert-manager.tf
@@ -23,10 +23,6 @@ resource "kubernetes_secret" "tls_route53_secret" {
 resource "kubernetes_namespace" "cert_manager" {
   metadata {
     name = "cert-manager"
-
-    labels = {
-      "certmanager.k8s.io/disable-validation" = "true"
-    }
   }
 }
 
@@ -183,12 +179,6 @@ resource "helm_release" "project_cert_manager" {
       defaultIssuerGroup: cert-manager.io
   EOF
   ]
-
-  # diable webhook to avoid error using stable/cert-manager: https://github.com/jetstack/cert-manager/issues/1255#issuecomment-465129995
-  set {
-    name  = "webhook.enabled"
-    value = "false"
-  }
 
   set {
     name = "installCRDs"
