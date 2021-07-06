f11a045064e0bdacc92c354f6df8d2b91a114321
Shaung Cheng <shaungc@umich.edu>
2021-07-06 00:29:43 -0700

    bump up microservice module version

--- terraform/code-server.tf
+++ terraform/code-server.tf
@@ -1,6 +1,6 @@
 module "code_server" {
   source  = "rivernews/kubernetes-microservice/digitalocean"
-  version = ">= v0.1.29"
+  version = ">= v0.1.30"
 
   aws_region     = var.aws_region
   aws_access_key = var.aws_access_key
