fce0bf5883f4b3440be0d70199dd836bdc6cfc29
Shaung <shaungc@umich.edu>
2022-10-24 00:03:49 -0700

    disable microservices

--- terraform/microservices.tf
+++ terraform/microservices.tf
@@ -68,41 +68,41 @@ module "redis_cluster" {
   ]
 }
 
-module "code_server" {
-  source  = "rivernews/kubernetes-microservice/digitalocean"
-  version = ">= v0.2.1"
+# module "code_server" {
+#   source  = "rivernews/kubernetes-microservice/digitalocean"
+#   version = ">= v0.2.1"
 
-  aws_region     = var.aws_region
-  aws_access_key = var.aws_access_key
-  aws_secret_key = var.aws_secret_key
-  cluster_name   = data.digitalocean_kubernetes_cluster.project_digitalocean_cluster.name
-  node_pool_name = data.digitalocean_kubernetes_cluster.project_digitalocean_cluster.node_pool.0.name
+#   aws_region     = var.aws_region
+#   aws_access_key = var.aws_access_key
+#   aws_secret_key = var.aws_secret_key
+#   cluster_name   = data.digitalocean_kubernetes_cluster.project_digitalocean_cluster.name
+#   node_pool_name = data.digitalocean_kubernetes_cluster.project_digitalocean_cluster.node_pool.0.name
 
-  app_label           = "code-server"
-  app_exposed_port    = 8003
-  app_deployed_domain = "code-server.shaungc.com"
+#   app_label           = "code-server"
+#   app_exposed_port    = 8003
+#   app_deployed_domain = "code-server.shaungc.com"
 
-  app_container_image     = "shaungc/code-server"
-  app_container_image_tag = "3.10.2"
+#   app_container_image     = "shaungc/code-server"
+#   app_container_image_tag = "3.10.2"
 
-  app_secret_name_list = [
-    "/service/code-server/PASSWORD",
-    "/service/code-server/CODE_SERVER_PORT",
-    "/service/code-server/CODE_SERVER_VOLUME_MOUNT",
-    "/app/appl-tracky/ADMINS"
-  ]
+#   app_secret_name_list = [
+#     "/service/code-server/PASSWORD",
+#     "/service/code-server/CODE_SERVER_PORT",
+#     "/service/code-server/CODE_SERVER_VOLUME_MOUNT",
+#     "/app/appl-tracky/ADMINS"
+#   ]
 
-  persistent_volume_mount_setting_list = [{
-    mount_path_secret_name = "/service/code-server/CODE_SERVER_VOLUME_MOUNT"
-    size = "3Gi"
-  }]
+#   persistent_volume_mount_setting_list = [{
+#     mount_path_secret_name = "/service/code-server/CODE_SERVER_VOLUME_MOUNT"
+#     size = "3Gi"
+#   }]
 
-  memory_guaranteed = "400Mi"
-  memory_max_allowed = "1Gi"
+#   memory_guaranteed = "400Mi"
+#   memory_max_allowed = "1Gi"
 
-  enable_docker_socket = true
+#   enable_docker_socket = true
 
-  depend_on = [
-    helm_release.project-nginx-ingress
-  ]
-}
+#   depend_on = [
+#     helm_release.project-nginx-ingress
+#   ]
+# }
